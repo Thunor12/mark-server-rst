e70a2f3756677767c839e9c8e1483930a46bd4b4
Lucas SPITERI <lucasspiteri@protonmail.com>
2022-03-07 14:50:44 +0100

    Seperated structures in files

--- src/main.rs
+++ src/main.rs
@@ -1,154 +1,37 @@
-use std::io;
-use std::io::BufReader;
-use std::sync;
-use std::sync::atomic::Ordering::Relaxed;
+pub mod particle;
+pub mod ue_groups;
 
-use std::io::prelude::*;
-use std::io::BufWriter;
-use std::fs::File;
+pub use crate::particle::*;
+pub use crate::ue_groups::*;
 
-use actix_web::dev::Factory;
-use actix_web::web::Buf;
-use actix_web::{error, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use serde;
-use serde::__private::de::IdentifierDeserializer;
-use serde_json;
-use serde::Serialize;
-use serde::Deserialize;
-use atomic_float::{self, AtomicF64};
+use std::{
+    // io::{self, BufReader, prelude::*, BufWriter},
+    io::{BufReader, BufWriter},
+    // sync::{self, atomic::Ordering::Relaxed},
+    fs::File
+};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Particle {
+// use serde::{
+//     Serialize,
+//     Deserialize
+// };
+
+/*
+use actix_web::{
+    web::Buf,
+    error,
+    App, HttpResponse,
+    HttpServer,
+    Responder,
+    HttpRequest,
+    dev::Factory
+};
+*/
+
+struct Course {
     name: String,
-    weight: Option<f64>,
-    average: Option<f64>,
-    particles: Vec<Particle>,
-}
-
-impl Default for Particle {
-    fn default() -> Self {
-        Particle {
-            name: "".to_string(),
-            weight: Some(1 as f64),
-            average: None,
-            particles: Vec::new(),
-        }
-    }
-}
-
-impl FromIterator<Particle> for Particle {
-    fn from_iter<I: IntoIterator<Item=Particle>>(iter: I) -> Self {
-        let mut u = Self::default();
-
-        u.particles.extend(iter);
-        u.update();
-        u
-    }
-}
-
-impl Particle {
-    fn set_weight(&mut self, v: f64) {
-        self.weight = Some(v);
-    }
-
-    fn get_weight(& self) -> f64 {
-        match self.weight {
-            Some(w) => w,
-            None => 1 as f64,
-        }
-    }
-
-    fn get_average(& self) -> Result<f64, Box<dyn std::error::Error>> {
-        let e: Box<dyn std::error::Error> = From::from("No average available".to_string());
-        self.average.ok_or(e)
-    }
-
-    fn new(n: String, weight: f64, average: f64) -> Self {
-        Particle {
-            name: n,
-            average: Some(average),
-            weight: Some(weight),
-            particles: vec![],
-        }
-    }
-
-    // TODO add error handeling
-    fn update(&mut self) {
-        if self.particles.len() == 0 {
-            return;
-        }
-
-        let mut numerator = AtomicF64::new(0.0);
-        let mut summed_weights = AtomicF64::new(0.0);
-
-        self.particles
-            .as_mut_slice()
-            .into_par_iter()
-            .for_each(|p| {p.update();});
-
-        self.particles
-            .as_mut_slice()
-            .into_par_iter()
-            .filter(|p| {p.average.is_some()})
-            .for_each(|particle| {
-                let w = particle.get_weight();
-                numerator.fetch_add(particle.get_average().unwrap() * w, Relaxed);
-                summed_weights.fetch_add(w, Relaxed);
-            });
-
-        self.average = Some(*numerator.get_mut() / *summed_weights.get_mut());
-    }
-
-}
-
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct ExameGroup {
-    name: String,
-    particle: Particle,
-}
-
-impl Default for ExameGroup {
-    fn default() -> Self {
-        ExameGroup {
-            name: "".to_string(),
-            particle: Particle::default(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct UEGroup {
-    name: String,
-    particle: Particle,
-    passed: Option<bool>,
-    requirement: f64,
-}
-
-impl Default for UEGroup {
-    fn default() -> Self {
-        UEGroup {
-            name: "".to_string(),
-            particle: Particle::default(),
-            passed: None,
-            requirement: 10 as f64,
-        }
-    }
-}
-
-impl UEGroup {
-    fn determin_if_passed(&mut self) -> Option<bool> {
-        self.particle.update();
-        match self.particle.average {
-            Some(a) => {
-                self.passed = Some(a >= self.requirement);
-                return self.passed;
-            },
-            None => None,
-        }
-
-    }
+    ues: Vec<UEGroup>,
+    id: usize,
 }
 
 // async fn get_particle(req: HttpRequest) -> impl Responder {
@@ -231,82 +114,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[cfg(test)]
-mod tests {
-    use std::vec;
-    use crate::Particle;
-    use crate::UEGroup;
-
-    #[test]
-    #[should_panic]
-    fn test_panick_if_average_is_none() {
-        let p = Particle::default();
-        let a = p.get_average().unwrap();
-    }
-
-    #[test]
-    fn test_can_access_average_if_some() {
-        let p = Particle::new("".to_string(), 1.0, 1.0);
-        assert_eq!(p.get_average().unwrap(), 1.0)
-    }
-
-    #[test]
-    fn test_average_update_on_one_particle() {
-        let p = Particle::from_iter(
-            vec![
-                Particle::new("".to_string(), 1.0, 1.0),
-                Particle::new("".to_string(), 2.0, 4.0),
-            ]
-        );
-
-        assert_eq!(p.average.unwrap(), 3.0);
-    }
-
-    #[test]
-    fn test_average_update_cascade_particle() {
-        let mut p1 = Particle::from_iter(
-            vec![
-                Particle::new("".to_string(), 1.0, 1.0),
-                Particle::new("".to_string(), 2.0, 1.0),
-            ]
-        );
-        p1.weight = Some(1.0);
-
-        let mut p2 = Particle::new("".to_string(), 2.0, 4.0);
-
-        let p = Particle::from_iter(vec![p1, p2]);
-
-        assert_eq!(p.average.unwrap(), 3.0);
-    }
-
-    #[test]
-    fn test_uegroup_passes_with_requirement() {
-        let mut ue1 = UEGroup {
-            name: "Sciences".to_string(),
-            particle: Particle::from_iter(
-                vec![
-                    Particle::new("".to_string(), 1.0, 10.0),
-                    Particle::new("".to_string(), 2.0, 10.5),
-                ]),
-            passed: None,
-            requirement: 10.0,
-        };
-
-        assert_eq!(ue1.determin_if_passed().unwrap(), true);
-
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_uegroup_panic_if_none() {
-        let mut ue1 = UEGroup {
-            name: "Sciences".to_string(),
-            particle: Particle::from_iter(vec![]),
-            passed: None,
-            requirement: 10.0,
-        };
-
-        ue1.determin_if_passed().unwrap();
-
-    }
-
-}
+pub mod test;
